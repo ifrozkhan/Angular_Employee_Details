e592245e1060b1b9018b834f25e819af1e2408f5
nayazi ifroz khan <108205007+ifrozkhan@users.noreply.github.com>
2026-01-16 16:48:51 +0530

    Updated the validators to Rective form validators

--- src/app/pages/employee-details/employee-details.component.ts
+++ src/app/pages/employee-details/employee-details.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject, NgModule, OnInit } from '@angular/core';
 import { EmployeeDetailsModel } from '../../model/Employee';
-import { FormGroup, FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { NgIf, NgFor  } from '@angular/common';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-employee-details',
@@ -10,62 +10,75 @@ import { NgIf, NgFor  } from '@angular/common';
   templateUrl: './employee-details.component.html',
   styleUrl: './employee-details.component.css'
 })
-export class EmployeeDetailsComponent implements OnInit{
+export class EmployeeDetailsComponent implements OnInit {
 
   employeeDetailsList: EmployeeDetailsModel[] = [];
   empDetailsobj: EmployeeDetailsModel = new EmployeeDetailsModel();
   employeeDetailsForm: FormGroup = new FormGroup({});
   message: string = '';
-  genderList =['male', 'Female'];
- title = 'API Employee Details';
+  genderList = ['male', 'Female'];
+  title = 'API Employee Details';
   http = inject(HttpClient);
   isSave = true;
+  userForm!: FormGroup;
+  searchcontrol: FormControl = new FormControl('');
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
 
+    this.userForm = this.fb.group({
+      empId: ['',Validators.required,Validators.pattern(/^\d+$/)],
+      name: ['', Validators.required, Validators.pattern(/^[a-zA-Z\s]+$/)],
+      type: ['', Validators.required, Validators.pattern(/^[a-zA-Z\s]+$/)],
+      gender: ['', Validators.required],
+      dob: ['', Validators.required],
+      address: ['', Validators.required, Validators.pattern(/^\S(.+)$/)]
+    });
   }
+
   ngOnInit(): void {
     this.getEmployeeDetails();
   }
 
   getEmployeeDetails() {
     this.http.get("https://localhost:7199/api/Employee_Details").subscribe((res: any) => {
-      this.employeeDetailsList =res;
+      this.employeeDetailsList = res;
     }
-  )}
+    )
+  }
 
-  onSave(form: NgForm){
-    if(!form || !form.valid){
-      this.showMessage('Please fill all required fields');
-      return;
-    }
-    this.http.post<EmployeeDetailsModel[]>("https://localhost:7199/api/Employee_Details", this.empDetailsobj).subscribe((res: any) => {
-      this.getEmployeeDetails(); this.onReset(form);
-      this.showMessage('Data save successfully')
+  onSave(userForm: any) {
+    if (this.userForm.valid) {
+      this.http.post<EmployeeDetailsModel[]>("https://localhost:7199/api/Employee_Details", this.empDetailsobj).subscribe((res: any) => {
+      this.getEmployeeDetails(); this.onReset(userForm);
+      this.message='Data save successfully';
+      this.userForm.reset();
     }, err => {
       console.error(err);
       this.showMessage('Error saving data');
     })
+      return;
+    }
+   
   }
 
-  onEdit(id: number){
+  onEdit(id: number) {
     this.isSave = false;
-    this.http.get<EmployeeDetailsModel>("https://localhost:7199/api/Employee_Details/"+id).subscribe((res: any) => {
+    this.http.get<EmployeeDetailsModel>("https://localhost:7199/api/Employee_Details/" + id).subscribe((res: any) => {
       debugger;
       this.empDetailsobj = res;
     })
   }
 
-  onUpdate(form: NgForm){
-    if(!form || !form.valid){
+  onUpdate(userForm: NgForm) {
+    if (!userForm || !userForm.valid) {
       this.showMessage('Please fill all required fields');
       return;
     }
-    this.http.put("https://localhost:7199/api/Employee_Details/"+this.empDetailsobj.emp_ID, this.empDetailsobj).subscribe((res: any)=>{
+    this.http.put("https://localhost:7199/api/Employee_Details/" + this.empDetailsobj.emp_ID, this.empDetailsobj).subscribe((res: any) => {
       debugger;
       this.getEmployeeDetails();
       this.isSave = true;
-      this.onReset(form);
+      this.onReset(userForm);
       this.showMessage('Data updated successfully')
     }, err => {
       console.error(err);
@@ -73,22 +86,23 @@ export class EmployeeDetailsComponent implements OnInit{
     })
   }
 
-  onDelete(id: number){
-    const result =confirm("Are you sure want to delete");
-    if(result){
-      this.http.delete<EmployeeDetailsModel>("https://localhost:7199/api/Employee_Details/"+id).subscribe((res:EmployeeDetailsModel) => {
+  onDelete(id: number) {
+    const result = confirm("Are you sure want to delete");
+    if (result) {
+      this.http.delete<EmployeeDetailsModel>("https://localhost:7199/api/Employee_Details/" + id).subscribe((res: EmployeeDetailsModel) => {
         this.getEmployeeDetails();
         const result = alert("Data deleted Successfully");
       })
     }
   }
-  onReset(form: NgForm){
+  onReset(userForm: NgForm) {
     this.empDetailsobj = new EmployeeDetailsModel();
-    if(form) form.resetForm();
+    this.isSave = true;
+    if (userForm) userForm.resetForm();
     this.showMessage('Data reseted successfully');
     //   this.empobj ={
-  //     id: undefined, empId: 0, name : "", city : "", state : "", emailId : "", contactNO : "", address : "", pincode : ""
-  //   };
+    //     id: undefined, empId: 0, name : "", city : "", state : "", emailId : "", contactNO : "", address : "", pincode : ""
+    //   };
   }
   showMessage(msg: string){
     try{
@@ -99,5 +113,5 @@ export class EmployeeDetailsComponent implements OnInit{
     setTimeout(()=> {
       this.message ='';
     }, 3000)
-    }
+  }
 }
