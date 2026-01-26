5bc1eaae15fdb38a1b57b53b217b08ca05668795
nayazi ifroz khan <108205007+ifrozkhan@users.noreply.github.com>
2026-01-26 16:35:41 +0530

    Implemented Ngrx basic store

--- src/app/ngrx-store/counter.action.ts
+++ src/app/ngrx-store/counter.action.ts
@@ -1,4 +1,13 @@
+// Topic: Ngrx basic store
 //Operation you sre going to perform on data
+// action - create action file
+// reducer - create reducer file for logic
+// Store - inject service in config file
+// dispatch - dispatch operation for the data execution
+// select - display a data in different components
+
+//Operation you sre going to perform on data
+
 import {createAction} from "@ngrx/store" 
 
 export const increment = createAction('[Counter] increment action');
